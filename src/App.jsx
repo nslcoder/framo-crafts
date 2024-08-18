1e2f4c37266391c31e7ab2f9a8ae793d642c9750
nslcoder <lokeshgurungwj@gmail.com>
2024-08-18 12:26:37 +0545

    Initiate framo-crafts

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <>
+      <div className="bg-slate-300 text-blue-500 p-10">
+        <h1 className="text-3xl">This is a example title</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam
+          consequuntur consectetur tempora recusandae autem quas mollitia quod
+          laboriosam laborum et. Veniam laborum perferendis, est reprehenderit
+          suscipit voluptas eveniet nam fuga?
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
